ea2b4a56d30236f53c4fb19fbb1937647258cef1
Cixi Geng <cixi.geng1@unisoc.com>
2020-11-17 16:17:55 +0800

    add dpkg pack functions
    Signed-off-by: Cixi Geng <cixi.geng1@unisoc.com>

--- install.sh
+++ install.sh
@@ -6,9 +6,25 @@
 #########################################################################
 #!/bin/bash
 
-GCOV_BIN="$PWD"
+GCOV_BIN="$PWD/fakeroot/usr/bin"
 
-cp lcovrc ~/.lcovrc
-echo "export GCOV_BIN=\"${GCOV_BIN}\"" >> ~/.bashrc
-echo "export PATH=\${GCOV_BIN}:\$PATH" >> ~/.bashrc
+SET_PARA1="export GCOV_BIN=\"${GCOV_BIN}\""
+grep "$SET_PARA1" ~/.bashrc && sed -i "/export GCOV_BIN=/d" ~/.bashrc || echo $SET_PARA1 >> ~/.bashrc
+SET_PARA2="export PATH=\${GCOV_BIN}:\$PATH"
+grep "$SET_PARA2" ~/.bashrc && sed -i "/export PATH=\${GCOV_BIN}/d" ~/.bashrc || echo $SET_PARA2 >> ~/.bashrc
+
+#echo "export GCOV_BIN=\"${GCOV_BIN}\"" >> ~/.bashrc
+#echo "export PATH=\${GCOV_BIN}:\$PATH" >> ~/.bashrc
 source ~/.bashrc
+cp lcovrc ~/.lcovrc
+generate_deb_package()
+{
+	cd fakeroot
+	md5sum `find usr -type f` > DEBIAN/md5sums
+	cd ..
+	if [ -f *.deb ];then
+		rm *.deb
+	fi
+	dpkg -b fakeroot lcov-kernel.deb
+}
+
